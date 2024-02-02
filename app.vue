9d8ce825919cae32b9396f646e86479f5086322e
Guilherme Ytalo <gydaytalo@gmail.com>
2024-02-01 22:29:09 -0300

    feat: started card layout

--- app.vue
+++ app.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <!-- <NuxtWelcome /> -->
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button>teste</Button>
+    <CardComponent title="Total Balance" balance="47, 651" percents="12.5" month="October"/>
+    <CardComponent title="Sales" balance="23,124" percents="24.8" month="October"/>
   </div>
 </template>
+<script setup lang="ts">
+import CardComponent from "~/components/CardComponent.vue";
+</script>
---
+++ components/CardComponent.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from '@/components/ui/card'
+import {MoreHorizontal, TrendingUp} from 'lucide-vue-next';
+
+defineProps({
+  title: String,
+  balance: String,
+  percents: String,
+  month: String,
+})
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <Card class="w-[250px] rounded-3xl">
+      <CardHeader>
+        <div class="flex justify-between">
+          <CardDescription class="font-bold text-inherit">{{ title }}</CardDescription>
+          <MoreHorizontal/>
+        </div>
+        <div class="flex">
+          <CardTitle>$ {{ balance }}</CardTitle>
+          <span class="ml-2 rounded-full bg-white px-2 py-1 text-xs flex items-center">
+            <TrendingUp class="text-green-500 mr-1 w-4"/>
+            +{{ percents }}%
+          </span>
+        </div>
+        <CardDescription class="font-bold text-inherit">in {{ month }}</CardDescription>
+      </CardHeader>
+    </Card>
+  </div>
+</template>
