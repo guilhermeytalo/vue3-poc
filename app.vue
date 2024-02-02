39ddd38e7bbe6b345333eea0397cc20b39fec65a
Guilherme Ytalo <gydaytalo@gmail.com>
2024-02-01 22:49:18 -0300

    feat: changed to v-for instead of 2 components

--- app.vue
+++ app.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    <!-- <NuxtWelcome /> -->
-    <CardComponent title="Total Balance" balance="47, 651" percents="12.5" month="October"/>
-    <CardComponent title="Sales" balance="23,124" percents="24.8" month="October"/>
+    <!-- Card Container-->
+    <div class="flex ml-16">
+      <CardComponent
+          v-for="(card, index) in cards"
+          :key="index"
+          :title="card.title"
+          :balance="card.balance"
+          :percents="card.percents"
+          :month="card.month"
+      />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
+import {reactive} from 'vue';
 import CardComponent from "~/components/CardComponent.vue";
+
+const cards = reactive([
+  {title: "Total Balance", balance: "47, 651", percents: "12.5", month: "October"},
+  {title: "Sales", balance: "23,124", percents: "24.8", month: "October"},
+]);
 </script>
